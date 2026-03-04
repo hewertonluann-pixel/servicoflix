694d616b870f06d13e21898ad4100cd15b169ca7
hewertonluann-pixel <hewertonluann@gmail.com>
2026-03-04 12:58:17 -0300

    feat: adiciona rota para edição de perfil do prestador

--- src/App.tsx
+++ src/App.tsx
@@ -10,6 +10,7 @@ import { SearchPage } from '@/pages/SearchPage'
 import { SimpleLoginPage } from '@/pages/SimpleLoginPage'
 import { AdminPage } from '@/pages/AdminPage'
 import { SettingsPage } from '@/pages/SettingsPage'
+import { EditProviderProfilePage } from '@/pages/EditProviderProfilePage'
 
 function App() {
   const location = useLocation()
@@ -22,6 +23,7 @@ function App() {
         <Route path="/" element={<HomePage />} />
         <Route path="/profissional/:id" element={<ProviderProfilePage />} />
         <Route path="/meu-perfil" element={<ProviderDashboardPage />} />
+        <Route path="/meu-perfil/editar" element={<EditProviderProfilePage />} />
         <Route path="/prestador/solicitacoes" element={<ProviderRequestsPage />} />
         <Route path="/minha-conta" element={<MyAccountPage />} />
         <Route path="/configuracoes" element={<SettingsPage />} />
