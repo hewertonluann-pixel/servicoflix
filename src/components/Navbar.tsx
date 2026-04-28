1a39a603d2771b813d5b01a410b878e0292b7ba0
hewertonluann-pixel <hewertonluann@gmail.com>
2026-04-27 22:06:37 -0300

    Simplify body style management on menu toggle
    Refactor scroll handling for menu open state.

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -57,23 +57,12 @@ export const Navbar = () => {
 
 useEffect(() => {
     if (menuOpen) {
-      const scrollY = window.scrollY
-      document.body.style.position = 'fixed'
-      document.body.style.top = `-${scrollY}px`
-      document.body.style.width = '100%'
+      document.documentElement.style.overflow = 'hidden'
     } else {
-      const scrollY = document.body.style.top
-      document.body.style.position = ''
-      document.body.style.top = ''
-      document.body.style.width = ''
-      if (scrollY) window.scrollTo(0, parseInt(scrollY || '0') * -1)
+      document.documentElement.style.overflow = ''
     }
     return () => {
-      const scrollY = document.body.style.top
-      document.body.style.position = ''
-      document.body.style.top = ''
-      document.body.style.width = ''
-      if (scrollY) window.scrollTo(0, parseInt(scrollY || '0') * -1)
+      document.documentElement.style.overflow = ''
     }
   }, [menuOpen])
 
